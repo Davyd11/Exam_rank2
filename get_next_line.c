db76b4aef5e7dbdcfc585264d9dcc6f55d91f5a6
David <david.puente.r@gmail.com>
2020-07-16 11:06:02 +0200

    get next para examen

--- get_next_line.c
+++ get_next_line.c
@@ -3,137 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 12:35:40 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/07/02 13:13:01 by rcabezas         ###   ########.fr       */
+/*   Updated: 2020/07/16 11:03:26 by davyd11          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 
-size_t  ft_strlen(char *s)
+#include "get_next_line.h"
+
+int ft_strlen(char *s)
 {
-    size_t  i;
+    int i;
+    
     i = 0;
     while (s[i] != '\0')
         i++;
-    return (i);
+    return(i);
 }
 
-char    *ft_strchr(char *str, int c)
-{
-    unsigned int    i;
-    unsigned int    length;
-    
-    i = 0;
-    if (!str)
-        return (NULL);
-    length = ft_strlen(str);
-    while (*str != '\0' && *str != c)
-    {
-        str++;
-        i++;
-    }
-    if (*str != c && i == length)
-        return (NULL);
-    return (str);
-}
-
-char    *ft_strjoin(char *s1, char *s2)
-{
-    char    *dest;
-    int     i;
-    
-    i = 0;
-    if (!(dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
-        return (NULL);
-    while (*s1 != '\0')
-        dest[i++] = *s1++;
-    while (*s2 != '\0')
-        dest[i++] = *s2++;
-    dest[i] = '\0';
-    return (dest);
-}
-
-char    *ft_strdup(char *str)
+char *ft_strdup(char *s)
 {
     int i;
     char *dest;
+    char *tmp;
+
     i = 0;
-    if (!(dest = malloc(ft_strlen(str) + 1)))
-        return (NULL);
-    while (str[i] != '\0')
+    if (!(tmp = malloc(ft_strlen(s) + 1)))
+        return (0);
+    while (s[i] != '\0')
     {
-        dest[i] = str[i];
+        tmp[i] = s[i];
         i++;
     }
-    dest[i] = '\0';
+    tmp[i] = '\0';
+    dest = tmp;
+    free(tmp);
     return (dest);
 }
 
-int     getcharpls(char **line, char **aux, int n)
+char *ft_strjoin(char *s, char *c)
 {
-    char *temp1;
-    char *temp2;
-    
-    if (n < 0)
-    {
-        if (*aux != NULL)
-        {
-            free(*aux);
-            *aux = NULL;
-        }
-        return (-1);
-    }
-    if (!n && !*aux)
-    {
-        *line = ft_strdup("");
+    int i;
+    char *dest;
+    char *tmp;
+
+    i = 0;
+    if (!(tmp = malloc(ft_strlen(s) + 2)))
         return (0);
-    }
-    if ((temp1 = ft_strchr(*aux, '\n')))
+    while (s[i] != '\0')
     {
-        *temp1 = 0;
-        *line = ft_strdup(*aux);
-        temp2 = ft_strdup(++temp1);
-        free(*aux);
-        *aux = temp2;
+        tmp[i] = s[i];
+        i++;
     }
-    else
-    {
-        *line = ft_strdup(*aux);
-        free(*aux);
-        *aux = NULL;
-        return (0);
-    }
-    return (1);
+    tmp[i] = c[0];
+    tmp[i + 1] = '\0';
+    dest = tmp;
+    free(tmp);
+    return (dest);
 }
 
-int     get_next_line(char **line)
+int get_next_line(char **line)
 {
-    static char     *aux;
-    char            buffer[65];
-    char            *temp;
-    int             n;
-    if (!line)
+    char buf[1];
+    int ret;
+
+    if (line == '\0' || !(*line = ft_strdup("")))
         return (-1);
-    while (ft_strchr(aux, '\n') == NULL)
+    while ((ret = read(0, buf, 1)) > 0)
     {
-        n = read(0, buffer, 64);
-        if (n < 0)
-            return (-1);
-        if (n == 0)
-            break ;
-        buffer[n] = 0;
-        if (!aux)
-            aux = ft_strdup(buffer);
-        else
+        if (buf[0] == '\n' || buf[0] == '\0')
         {
-            temp = ft_strjoin(aux, buffer);
-            free(aux);
-            aux = temp;
+            buf[0] = '\0';
+            break ;
         }
+        *line = ft_strjoin(*line, buf);
     }
-    return (getcharpls(line, &aux, n));
+    buf[0] = '\0';
+    return (ret);
 }
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 09:44:11 by davyd11           #+#    #+#             */
+/*   Updated: 2020/07/16 11:05:20 by davyd11          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+int get_next_line(char **line);
